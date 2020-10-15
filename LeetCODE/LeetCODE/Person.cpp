165999c9d5c1741dfce7295905b7da826700b2b8
jeremylu <jingya.lu-a1079@aqara.com>
2020-10-15 19:55:20 +0800

    git rebase

--- LeetCODE/LeetCODE/Person.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  Person.cpp
-//  LeetCODE
-//
-//  Created by jeremy on 2020/9/30.
-//
-
-#include "Person.hpp"
---
+++ LeetCODE/LeetCODE/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  LeeTCode
+//
+//  Created by jeremy on 2020/10/13.
+//
+
+#include <iostream>
+using namespace std;
+const int MAX = 3;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    int var[MAX] = {20,33,25};
+    int *ptr;
+    ptr = var;
+    for (int i =0; i<MAX; i++) {
+        cout<<"address of var["<<i<<"] = ";
+        cout<<ptr<<endl;
+        cout<<"var["<<i<<"] =";
+        cout<<*ptr<<endl;
+        ptr++;
+    }
+    
+    return 0;
+}
