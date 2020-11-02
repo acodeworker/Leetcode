5203cd45734553d85505635bf49be71c6b9de3bc
jeremylu <jingya.lu-a1079@aqara.com>
2020-11-02 20:11:19 +0800

    update

--- MyPlayground.playground/Pages/ReverseNode.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/ReverseNode.xcplaygroundpage/Contents.swift
@@ -1,2 +1,12 @@
 
 import Foundation
+
+func missingNumber(_ nums: [Int]) -> Int {
+       let count = nums.count
+       // let target = 0
+    var total = 0
+    for index in 0...(count-1) {
+          total += nums[index] - index
+       }
+       return -total
+   }
