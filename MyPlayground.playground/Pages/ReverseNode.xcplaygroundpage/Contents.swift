bee9b463e5bd75e212baeb9a392229bbe9c99e6e
jeremylu <jingya.lu-a1079@aqara.com>
2020-11-02 21:22:35 +0800

    update

--- MyPlayground.playground/Pages/ReverseNode.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/ReverseNode.xcplaygroundpage/Contents.swift
@@ -1,12 +1,30 @@
 
 import Foundation
 
-func missingNumber(_ nums: [Int]) -> Int {
-       let count = nums.count
-       // let target = 0
-    var total = 0
-    for index in 0...(count-1) {
-          total += nums[index] - index
-       }
-       return -total
-   }
+func subsets(_ nums: [Int]) -> [[Int]] {
+    var array = [[Int]]()
+    var zeroValue = [Int]()
+    array.append(zeroValue)
+    var temp = [[Int]]()
+    for element in nums {
+        temp = array
+        for eleArray in temp {
+            eleArray.append(element)
+        }
+     }
+    
+    return array
+    }
+/**
+ 数组
+ 1.两数之和
+  268. 缺失数字
+  78.subsets
+  90子集
+ Linked List链表
+  206反转链表
+  21合并两个有序链表
+  02两数相加
+   24两两交换表中的节点
+ 
+ */
